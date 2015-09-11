0da03d5a9ba63f806d9d770479eefc0abefc10eb
gentlerebel <gbolahanalade@gmail.com>
2015-09-11 14:57:57 +0100

    pages styled - update 3
    search page styled
    businesses page styled

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,7 +14,8 @@
   
   <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
   <link rel="stylesheet" href="{{ asset('plugins/animate.css')}}">
-  
+  <link rel="stylesheet" href="{{ asset('plugins/selectize/selectize.default.css')}}">
+  <link href="{{asset('plugins/select2/select2.min.css')}}" rel="stylesheet">
 
   <!-- GOOGLE FONTS -->
   <!-- <link href='http://fonts.googleapis.com/css?family=Raleway:400,700,600,800%7COpen+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
@@ -50,8 +51,69 @@
   <script src="{{asset('js/jquery-2.1.3.min.js') }}"></script>
   <script src="{{asset('js/jquery-ui.js') }}"></script>
   <script src="{{asset('js/jquery.ba-outside-events.min.js') }}"></script>
+  <script src="{{asset('plugins/selectize/selectize.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('plugins/select2/select2.min.js')}}"></script>
   <!-- Page Scripts -->
   <script type="text/javascript">
+
+      $(document).ready(function() {
+            // Enable location search
+            $('#location').selectize({
+                valueField: 'name',
+                labelField: 'name',
+                searchField: ['name'],
+                renrender:{
+                    option:function(item, escape) {
+                      return '<div><i class="fa fa-map-marker"></i>' + ' ' + escape(item.name) +'</div>';
+                    }
+                  },
+                  load:function(query, callback){
+                    if(!query.length) return callback();
+                    $.ajax({
+                      url: '{{URL::to('api/location')}}',
+                      type: 'GET',
+                      dataType: 'json',
+                      data: {
+                        l: query
+                      },
+                      success: function(res) {
+                        callback(res.data);
+                        } 
+                    });
+                  }
+              });
+
+              // Enable category search
+              $('#category').selectize({
+                valueField: 'name',
+                labelField: 'name',
+                searchField: ['name'],
+                render:{
+                  option:function(item, escape) {
+                    return '<div><i class="fa fa-home"></i>' + ' ' + escape(item.name) +'</div>';
+                  }
+                },
+                load:function(query, callback) {
+                  if(!query.length) return callback();
+                  $.ajax({
+                    url: '{{URL::to('api/category')}}',
+                    type: 'GET',
+                    dataType: 'json',
+                    data: {
+                      q: query
+                    },
+                    success: function(res) {
+                      callback(res.data);
+                      }
+                  });
+                }
+              });
+
+              $('#category3').select2({
+                placeholder: 'search category',
+                tags: true
+              });
+          });
        $(document).ready(function() {
               $("body").addClass('animated fadeIn');
               $("h2.page-title").addClass('animated zoomIn');
@@ -62,7 +124,9 @@
           });
        $(function() {
           $('.tab-pane:first-child ').addClass('active animated zoomIn');
-       })
+       });
+
+       
   </script>
   @yield('scripts')
 <!-- SCRIPTS ENDS -->
--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -9,14 +9,14 @@
                     
 
                     <div class="category-search">
-                      <select id="category" name="category" placeholder="Search by categories here..."></select> 
+                      <select id="category" name="category" placeholder="Search keywords e.g. pizza, bars, restaurants..."></select> 
                     </div>
                     <span class="search-in">IN</span>
 
 
                     <div class="select-location input-group">
                       <!-- <span class="input-group-addon" id="basic-addon1"><i class="fa fa-home"></i></span> -->
-                      <select class="form-control" id="location" name="location" placeholder="Choose a city/area..."></select>                     
+                      <select class="" id="location" name="location" placeholder="Located in address, area, city, state..."></select>                     
                     </div>
 
                     <button class="search-btn" type="submit"><i class="fa fa-search"></i> <span class="hidden-lg hidden-md hidden-sm">Search</span></button>
