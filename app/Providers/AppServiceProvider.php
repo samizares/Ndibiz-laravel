105c65aa0685fb5923731c0b89999f825ec90985
Samuel-Junior Oghogho <samizares@hotmail.com>
2016-03-26 01:23:12 +0000

    Fixed global variable 3
    Fixed global variable 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,13 +19,13 @@ class AppServiceProvider extends ServiceProvider
             $view->stateListID=\App\State::lists('name','id');
             $view->catList= \App\Cat::lists('name','name');
             $view->catListID=\APP\Cat::lists('name','id');
-            $view->subListID=\App\Subcat::lists('name','id');
+            $view->subListID=\App\SubCat::lists('name','id');
             $view->featured= \App\Biz::whereFeatured('YES')->paginate(4);
             $view->recent= \App\Biz::orderBy('created_at', 'desc')->paginate(3);
             $view->cats = \App\Cat::all();
             $view->totalBiz=\App\Biz::count();
             $view->totalCat=\App\Cat::count();
-            $view->totalSubCat=\App\Subcat::count();
+            $view->totalSubCat=\App\SubCat::count();
         });
     }
 
