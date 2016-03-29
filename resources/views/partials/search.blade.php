9c27296822eb040e85e92526a1929dd2c84f3f9b
Developer <bolajialade.work@gmail.com>
2016-03-29 11:07:00 +0100

    Add Search Bar as overlay on Page banner for required pages.

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -5,12 +5,12 @@
                         {{--Keyword Search--}}
                         <ul class="list-inline m0 search-bar">
                             <li class="m0">
-                                <select type="text" aria-label="category" class="" id="category" name="category"
-                                      placeholder="Type a Keyword"></select>
+                                <select type="text" required="required" aria-label="category" class="" id="category" name="category"
+                                      placeholder="Type a Keyword..."></select>
                             </li>
                             {{--Location Search--}}
                             <li class="m0">
-                                <select type="text" class="" id="location" name="location" placeholder="Select a Location"></select>
+                                <select type="text" required="required" class="" id="location" name="location" placeholder="Select a Location"></select>
                             </li>
                             {{--Search Button--}}
                             <button class="btn btn-default-inverse" type="submit"> Find a Business <i class="fa fa-search"></i> </button>
